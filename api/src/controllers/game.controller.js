cd9eeb4fde6f03e22df59d509bd4eac147f85be2
Danny Hermoso <danny110298yaexiste@gmail.com>
2022-09-01 16:06:43 -0500

    rutas de juegos, script para database

--- api/src/controllers/game.controller.js
+++ api/src/controllers/game.controller.js
@@ -1,3 +1,68 @@
+const Game = require('../models/Game.js')
+
+const getGamesById = async (req, res) => {
+    const { id } = req.params
+    try {
+        const game = await Game.findById(id)
+
+        return res.status(200).json({ game: game })
+
+        console.log(game)
+    } catch (error) {
+        return res.status(400).json( { error: error })
+    }
+    
+}
 
 
 
+const getGenres = async (req, res) => {
+    const genres = [
+        'action',
+        'indie',
+        'adventure',
+        'rPG',
+        'strategy',
+        'shooter',
+        'casual',
+        'simulation',
+        'puzzle',
+        'arcade',
+        'platformer',
+        'racing',
+        'massively-multiplayer',
+        'sports',
+        'fighting',
+        'family',
+        'board Games',
+        'educational',
+        'card'
+    ]
+    try {
+        return res.status(200).json( { genres } )
+    } catch (error) {
+        return res.status(400).json( { error: error } )
+    }
+}
+
+
+const postGame = async (req, res) => {
+    const game = req.body
+    try {
+        await Game.create(game)
+        return res.status(200).json( { gameAdded: 'Game added'} )
+    } catch (error) {
+        return res.status(500).json( { error: error } )
+    }
+}
+
+const putGame = async (req, res) => {
+    const { idGame } = req.params
+    const game = req.body
+    try {
+        const gameUpdate = await Game.updateOne({ _id: idGame}, { $set: game })
+        res.status(200).json( { gameUpdate: gameUpdate } )
+    } catch (error) {
+        res.status(500).json( { error: error } )
+    } 
+}
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,30 +1,90 @@
 const { Router } = require('express');
 const Game = require('../models/Game.js')
+const axios = require('axios').default
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
 
 const router = Router();
 
-// Configurar los routers
-// Ejemplo: router.use('/auth', authRouter);
 
 
-router.get('/', async (req, res) => {
-    const games = await Game.find()
-    console.log(games)
-    res.json(games)
+
+const genres = [
+    'action',
+    'indie',
+    'adventure',
+    'rPG',
+    'strategy',
+    'shooter',
+    'casual',
+    'simulation',
+    'puzzle',
+    'arcade',
+    'platformer',
+    'racing',
+    'massively-multiplayer',
+    'sports',
+    'fighting',
+    'family',
+    'board Games',
+    'educational',
+    'card'
+]
+
+
+// router.get('/', async (req, res) => {
+
+//     genres.forEach(async (current) =>{
+//         const { data } = await axios.get(`https://api.rawg.io/api/games?key=8f18e9d52c1a4529b8ffba93f32936dd&genres=${current}&page_size=9`)
+
+//         data.results.forEach((gamer) =>{
+//             Game.create({
+//                 name: gamer.name,
+//                 background_image: gamer.background_image,
+//                 platforms: gamer.platforms.map( (current) => current.platform.name),
+//                 released: gamer.released,
+//                 rating: gamer.rating,
+//                 price: 0,
+//                 genre: current,
+                
+                
+//             })
+//             console.log(gamer.id)
+//             console.log(gamer.name)
+//             console.log(gamer.released)
+//             console.log(gamer.rating)
+//             console.log(gamer.platforms.map( (current) => current.platform.name))
+//             console.log(current)
+
+//         })
+        
+//     })
     
-})
 
-router.post('/', async ( req, res) => {
-    const { newGame } = req.body;
-    const game = new Game({ 
-        newGame 
-    })
-    console.log(game)
-    await game.save();
-    res.json({status: 'game added'})
-})
+
+    
+// })
+
+
+// router.get('/', async (req, res) => {
+//     const games = await Game.find()
+//     console.log(games)
+//     res.status(200).json(games)
+    
+// })
+
+
+
+
+// router.post('/', async ( req, res) => {
+//     const { newGame } = req.body;
+//     const game = new Game({ 
+//         newGame 
+//     })
+//     console.log(game)
+//     await game.save();
+//     res.json({status: 'game added'})
+// })
 
 module.exports = router;
