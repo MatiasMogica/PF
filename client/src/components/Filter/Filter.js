39bba71860e015ee4116e26909ff64e60fc4ab28
Angela Testino <angela11021997@gmail.com>
2022-09-05 04:41:05 -0500

    css avance

--- client/src/components/Filter/Filter.js
+++ client/src/components/Filter/Filter.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { filterVideogames } from "../../redux/slices/videogamesSlice";
+
 import './index.css'
 
 function Filtro() {
@@ -154,37 +155,38 @@ function Filtro() {
   // Creo que el html es self-explanatory...
   return (
     <div id="filtrobox" className="filtroContainer">
-      <div>
-        <h4>Search by keyword</h4>
+      <div className="container_search">
+        <h3>Search by keyword</h3>
 
         <label htmlFor="buscarfiltro">
         Enter keywords here to search for matches!{" "}
-        </label>
+        </label><br/><br/>
+        <div className="search">
         <input
           type="text"
           id="buscarfiltro"
           placeholder="Search"
           className="buscar"
           onChange={(e) => handleName(e)}
-        ></input>
+        ></input></div>
       </div>
 
       <div>
-        <h4>Release date:</h4>
+        <h3>Release date:</h3>
         <label htmlFor="fecha_min">From: </label>
         <input
           type="date"
           id="fecha_min"
           name="trip-start"
           onChange={(e) => handleReleasedMin(e)}
-        ></input>
+        ></input><br/>
         <label htmlFor="fecha_max">To: </label>
         <input
           type="date"
           id="fecha_max"
           name="trip-start"
           onChange={(e) => handleReleasedMax(e)}
-        ></input>
+        ></input><br/>
         <button onClick={(e) => handleReleased(e)} value="tba">
          Hasn't been announced yet?
         </button>
@@ -194,7 +196,7 @@ function Filtro() {
       </div>
 
       <div>
-        <h4>Genre</h4>
+        <h3>Genre</h3>
         {generos.map((x) => {
           return (
             <div key={x}>
@@ -212,7 +214,7 @@ function Filtro() {
       </div>
 
       <div>
-        <h4>Platform</h4>
+        <h3>Platform</h3>
         {plataforma.map((x) => {
           return (
             <div key={x}>
@@ -231,8 +233,8 @@ function Filtro() {
 
       <div>
         <div>
-          <h4>Price</h4>
-          <label htmlFor="precioMin">Price minimum</label>
+          <h3>Price</h3>
+          <label htmlFor="precioMin">Price minimum</label><br/>
           <input
             id="precioMin"
             type="number"
@@ -240,8 +242,8 @@ function Filtro() {
           ></input>
           <button onClick={(e) => handlePrecio(e)} value="sinminimo">
             No minimum?
-          </button>
-          <label htmlFor="precioMax">Price maximum</label>
+          </button><br/>
+          <label htmlFor="precioMax">Price maximum</label><br/>
           <input
             id="precioMax"
             type="number"
@@ -254,16 +256,16 @@ function Filtro() {
       </div>
 
       <div>
-        <h4>Sort by:</h4>
+        <h3>Sort by:</h3>
+        <div className='sortBy'>
+        <button className='button-64'role="button" onClick={(e) => handleOrderAlphabet(e)}><span class="text">Alphabet</span></button>
 
-        <button onClick={(e) => handleOrderAlphabet(e)}>Alphabetical</button>
+        <button className='button-64'role="button"  onClick={(e) => handleOrderRating(e)}><span class="text">Rating</span></button>
 
-        <button onClick={(e) => handleOrderRating(e)}>Rating</button>
-
-        <button onClick={(e) => handleOrderReleasedDate(e)}>
-          Released date
+        <button className='button-64' role="button" onClick={(e) => handleOrderReleasedDate(e)}>
+        <span class="text">Released <br/>date</span>
         </button>
-        <button onClick={(e) => handleOrderPrecio(e)}>Price</button>
+        <button className='button-64'role="button" onClick={(e) => handleOrderPrecio(e)}><span class="text">Price</span></button></div>
       </div>
     </div>
   );
