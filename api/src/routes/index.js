31334c210881e086742cdf04034d77e01345d155
Anderson <andersonventosilla0@gmail.com>
2022-09-18 13:23:39 -0500

    reviews added

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -4,6 +4,7 @@ const UserRouter = require("./user.routes.js");
 const AuthRouter = require("./authentication.routes");
 const SendEmailRouter = require("./sendemails.routes.js");
 const PaymentRouter = require("./payment.routes.js");
+const ReviewsRouter = require("./reviews.routes.js");
 
 const router = express.Router();
 
@@ -12,6 +13,7 @@ router.use("/payment", PaymentRouter);
 router.use("/users", UserRouter);
 router.use("/auth", AuthRouter);
 router.use("/email", SendEmailRouter);
+router.use("/reviews", ReviewsRouter);
 router.use("*/*", (req, res) => {
   try {
     res.status(404).send(`This page doesn't exists`);
