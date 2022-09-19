b08e00fd7d374da2b5b85eb7bdf9fb1e3099700a
Angela Testino <angela11021997@gmail.com>
2022-09-19 15:19:56 -0500

    arreglo navbar admin flujo

--- client/src/components/NavBar/index.js
+++ client/src/components/NavBar/index.js
@@ -8,24 +8,22 @@ import {
   NavLinkHome,
   NavLinkAdmin,
 } from "./NavBarStyle";
+import NavBarAdmin from "./NavBarAdmin"
 import { useDispatch, useSelector } from "react-redux";
 import LogOut from "../LogOut/LogOut";
 import { CartIcon } from "../../icons/Icons";
 import "./index.css";
 import { useLocation } from "react-router-dom";
-import { AiOutlineHome } from "react-icons/ai";
-import { FiUsers } from "react-icons/fi";
-import { IoGameControllerOutline } from "react-icons/io5";
 import { IncomingRequestsGetData } from "../../redux/actions/friendActions";
 import Dropdown from "react-bootstrap/Dropdown";
 import settingslogo from "../../icons/settings.ico";
 import bell from "../../icons/bell.png";
 import NavBarLogIn from "../NavBarLogin/NavBarLogIn";
 
-export default function NavBar({ usuario }) {
+export default function NavBar() {
   const user = useSelector((state) => state.logIn.logIn);
   const friendRequests = useSelector((state) => state.friend.incomingRequests);
-  const { pathname } = useLocation();
+
   let { amount } = useSelector((state) => state.cart);
   const dispatch = useDispatch();
   useEffect(() => {
@@ -37,33 +35,7 @@ export default function NavBar({ usuario }) {
   return (
     //
     <>
-      {pathname === "/adminPanel" ? (
-        <Container>
-          <ProfileContainer>
-            <Avatar src={user.image} />
-            <Name>{user.username}</Name>
-            <Div>
-              <LogOut></LogOut>
-            </Div>
-          </ProfileContainer>
-          <NavMenuAdmin>
-            <NavLinkHome to="/home">
-              <AiOutlineHome />
-              <HLinks>Home</HLinks>
-            </NavLinkHome>
-            <div className="user">
-              <BtnUser onClick={usuario}>
-                <FiUsers color="white" />
-                <HLinks>Users</HLinks>
-              </BtnUser>
-            </div>
-            <NavLink to="/videogame/add">
-              <IoGameControllerOutline />
-              <HLinks>Create</HLinks>
-            </NavLink>
-          </NavMenuAdmin>
-        </Container>
-      ) : (
+      
         <Nav>
           <NavLink to="/">
             <h1>ZTEAM</h1>
@@ -162,80 +134,8 @@ export default function NavBar({ usuario }) {
             <NavBarLogIn />
           )}
         </Nav>
-      )}
+    
     </>
   );
 }
-const Container = styled.div`
-  width: 20%;
-  min-height: 97vh;
-  border-radius: 2rem;
-  background-color: #545479;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  gap: 3rem;
-  align-self: flex-start;
-  position: sticky;
-  top: 0.7rem;
-  overflow: hidden;
-  width: 20%;
-`;
 
-const ProfileContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-`;
-const Avatar = styled.img`
-  height: 7rem;
-  border-radius: 6rem;
-  margin-top: 20%;
-`;
-const Name = styled.h3`
-  color: white;
-  font: 0.5rem;
-  font-weight: 400;
-  margin: 0.8rem 0 0.5rem 0;
-`;
-const Div = styled.div`
-  padding: 0.3rem 1rem;
-  border-radius: 1rem;
-  font-weight: 500;
-  color: white;
-  background-color: #5a8dd4;
-  cursor: pointer;
-`;
-const NavMenuAdmin = styled.div`
-  heigth: 100%;
-  width: 100%;
-  align-items: center;
-  justify-content: center;
-  border-radius: 2rem;
-  display: flex;
-  background-color: #545479;
-  flex-direction: column;
-  line-height: 3rem;
-  &:hover {
-    color: #81e6d9;
-  }
-`;
-const HLinks = styled.h3`
-  padding-left: 0.5rem;
-  color: #ffffff;
-  &:hover {
-    color: #81e6d9;
-  }
-`;
-const BtnUser = styled.button`
-  border: none;
-  background-color: transparent;
-  color: #FFFFF;
-  cursor: pointer;
-  appearance: none;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  font-size: 1rem;
-`;
