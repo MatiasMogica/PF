2626827ac8a4fdf32282fe8bb14fd0d02b64f1d8
Franco <juanfrancorodriguez99@gmail.com>
2022-09-19 22:33:44 -0300

    fixed navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import { Route, Switch, Redirect } from "react-router-dom";
 
 import Error404 from "./components/Errors/index";
@@ -17,12 +17,11 @@ import AdminPanel from "./components/AdminPanel/AdminPanel";
 import Edit from "./pages/Edit/Edit";
 import Payment from "./components/Payment/Payment";
 import CartContainer from "./components/Cart/CartContainer";
-import PurchaseOrders from "./components/PurchaseOrders/PurchaseOrders"
+import PurchaseOrders from "./components/PurchaseOrders/PurchaseOrders";
 import UserDetailsOptions from "./pages/UserDetails&Options/UserDetails&Options";
 import Settings from "./pages/Settings/Settings";
-
-import Users from "./components/AdminPanel/User"
-
+import GamesOwnedById from "./pages/GamesOwnedById/GamesOwnedById";
+import Users from "./components/AdminPanel/User";
 
 function App() {
   const user = useSelector((state) => state.logIn.logIn);
@@ -69,11 +68,14 @@ function App() {
 
         {user.admin ? (
           <>
-           
             <Route exact path={"/adminPanel"} component={AdminPanel} />
             <Route exact path={"/videogame/add"} component={Add} />
             <Route exact path={"/edit/:id"} component={Edit} />
-            <Route exact path={"/adminPanel/purchaseOrders"} component={PurchaseOrders} />
+            <Route
+              exact
+              path={"/adminPanel/purchaseOrders"}
+              component={PurchaseOrders}
+            />
             <Route exact path={"/adminPanel/user"} component={Users} />
             <Route exact path={"/users/:id"} component={UserDetailsOptions} />
           </>
