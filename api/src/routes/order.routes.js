fb69fe1ad23247dee339217e165ed29e412ccde0
Anderson <andersonventosilla0@gmail.com>
2022-09-21 19:41:12 -0500

    chart order added

--- api/src/routes/order.routes.js
+++ api/src/routes/order.routes.js
@@ -1,15 +1,24 @@
 const { Router } = require("express");
 
-const {postOrder,deleteOrder,allOrders,idOrder}=require("../controllers/order.controller")
+const {
+  postOrder,
+  deleteOrder,
+  allOrders,
+  idOrder,
+  getOrderStats,
+} = require("../controllers/order.controller");
 
 const router = Router();
 
-router.post('/postOrder', postOrder)
+router.post("/postOrder", postOrder);
 
-router.delete('/deleteOrder/:id', deleteOrder)
+router.delete("/deleteOrder/:id", deleteOrder);
 
-router.get('/order', allOrders)
+router.get("/order", allOrders);
 
-router.get('/order/:id', idOrder)
+router.get("/order/:id", idOrder);
 
-module.exports = router
+//ORDER STATS
+router.get("/stats", getOrderStats);
+
+module.exports = router;
--- client/src/components/PurchaseOrders/PurchaseOrders.js
+++ client/src/components/PurchaseOrders/PurchaseOrders.js
@@ -1,47 +1,137 @@
-
-import NavBarAdmin from "../NavBar/NavBarAdmin"
-import styled from "styled-components"
-import  {getOrders} from "../../redux/actions/orderActions";
-import { useDispatch,useSelector} from "react-redux";
-import { useEffect} from "react";
+import NavBarAdmin from "../NavBar/NavBarAdmin";
+import styled from "styled-components";
+import { getOrders } from "../../redux/actions/orderActions";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect, useMemo, useState } from "react";
 import CardOrder from "./CardOrder";
+import Chart from "../Chart/Chart";
+import axios from "axios";
 
-export default function PurchaseOrders(){
-    const order = useSelector((state) => state.orders.orders);
-    const dispatch=useDispatch()
-    useEffect(() => {  
-       dispatch(getOrders())
-    },[dispatch])
+export default function PurchaseOrders() {
+  const order = useSelector((state) => state.orders.orders);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getOrders());
+  }, [dispatch]);
 
-    
-    return (
-        <>
-        <Container>
-            <NavBarAdmin/>
-            <DivMainOrder>
-                {order && order.map((order) =>{
-                return <CardOrder key={order.id} {...order} />
-                })}
-            </DivMainOrder>
+  const data = [
+    {
+      name: "Jan",
+      "Active User": 4000,
+    },
+    {
+      name: "Feb",
+      "Active User": 3000,
+    },
+    {
+      name: "Mar",
+      "Active User": 5000,
+    },
+    {
+      name: "Apr",
+      "Active User": 4000,
+    },
+    {
+      name: "May",
+      "Active User": 3000,
+    },
+    {
+      name: "Jun",
+      "Active User": 2000,
+    },
+    {
+      name: "Jul",
+      "Active User": 4000,
+    },
+    {
+      name: "Agu",
+      "Active User": 3000,
+    },
+    {
+      name: "Sep",
+      "Active User": 4000,
+    },
+    {
+      name: "Oct",
+      "Active User": 1000,
+    },
+    {
+      name: "Nov",
+      "Active User": 4000,
+    },
+    {
+      name: "Dec",
+      "Active User": 3000,
+    },
+  ];
+  // ORDER STATS
+  const [orderStats, setOrderStats] = useState([]);
 
-        </Container>
-        </>
-    )
+  const MONTHS = useMemo(
+    () => [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Agu",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+    ],
+    []
+  );
+
+  useEffect(() => {
+    const getStats = async () => {
+      try {
+        const res = await axios.get("http://localhost:3001/order/stats");
+        res.data.map((item) => {
+          setOrderStats((prev) => [
+            ...prev,
+            { name: MONTHS[item._id - 1], "Monthly orders income": item.total },
+          ]);
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getStats();
+  }, [MONTHS]);
+
+  return (
+    <>
+      <Chart
+        data={orderStats}
+        title="Order Analytics"
+        grid
+        dataKey="Monthly orders income"
+      />
+      <Container>
+        <NavBarAdmin />
+        <DivMainOrder>
+          {order &&
+            order.map((order) => {
+              return <CardOrder key={order.id} {...order} />;
+            })}
+        </DivMainOrder>
+      </Container>
+    </>
+  );
 }
 const Container = styled.div`
   display: flex;
   border-radius: 2rem;
-  
-  
 `;
 const DivMainOrder = styled.div`
-display:flex;
-flex-direction: column;
-background-color:#202020;
-width:80%;
-height:fit-content;
-min-height:80%;
-margin:20px;
-
-
-`
+  display: flex;
+  flex-direction: column;
+  background-color: #202020;
+  width: 80%;
+  height: fit-content;
+  min-height: 80%;
+  margin: 20px;
+`;
